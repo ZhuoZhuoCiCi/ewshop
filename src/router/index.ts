ef90c7cb40f017d944f4dd27055eeb88785d445f
cici <cici@cici>
2023-02-15 19:36:28 +0800

    简化路径前缀

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import {createRouter,createWebHistory} from "vue-router";
-import Home from'../view/Home/Home.vue'
-import Login from'../view/Login/Login.vue'
+import Home from'@/view/Home/Home.vue'
+import Login from'@/view/Login/Login.vue'
 const routes=[
     {path:'/',component:Home},
     {path:'/login',component:Login}
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
-
+import { resolve } from 'path';
 // https://vitejs.dev/config/
+function pathResolve(dir:string){
+    // 获取绝对定位的方法
+    return resolve(process.cwd(), '.', dir);
+}
 export default defineConfig({
   plugins: [
       vue(),
@@ -11,4 +15,13 @@ export default defineConfig({
         resolvers:[NaiveUiResolver()]
       })
   ],
+    resolve:{
+      alias:[
+          {
+          // 简化路径前缀，使用绝对地址
+          find:'@',
+          replacement:(pathResolve('src'))
+      }
+      ]
+    },
 })
